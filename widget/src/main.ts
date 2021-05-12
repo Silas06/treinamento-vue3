907c855bc89aa25feff667d6e0408170d30d5d88
Silas Oliveira <silas.spo@hotmail.com>
2021-05-12 20:00:18 -0300

    update

--- widget/src/main.ts
+++ widget/src/main.ts
@@ -1,17 +1,18 @@
 import { createApp } from 'vue'
-import PlayGround from './views/PlayGround/index.vue'
+// import PlayGround from './views/PlayGround/index.vue'
 import App from './App.vue'
-import { setup } from './utils/bootstrap'
+// import { setup } from './utils/bootstrap'
 
 import '@/assets/css/tailwind.css'
 import '@/assets/css/fonts.css'
 import 'animate.css'
 
-setup({
-  onProduction: () => {
-    createApp(App).mount('#app')
-  },
-  onDevelopment: () => {
-    createApp(PlayGround).mount('#app')
-  }
-})
+// setup({
+//   onProduction: () => {
+//     createApp(App).mount('#app')
+//   },
+//   onDevelopment: () => {
+//     createApp(PlayGround).mount('#app')
+//   }
+// })
+createApp(App).mount('#app')
